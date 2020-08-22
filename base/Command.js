78bd3b735731c08f1fdf0ae8aea90b53c6bf2cc8
Nino <65510604+Nino-fr@users.noreply.github.com>
2020-08-23 01:59:29 +0200

    Add files via upload

---
+++ base/Command.js
@@ -0,0 +1,20 @@
+const { client } = require('../index.js');
+
+class Command {
+  constructor({
+    name = null,
+    description = 'Aucune',
+    category = 'Utilitaires',
+    usage = 'Aucun',
+    enabled = true,
+    guildOnly = false,
+    aliases = new Array(),
+    permLevel = 'User',
+  }) {
+    this.client = client;
+    this.conf = { enabled, guildOnly, aliases, permLevel };
+    this.help = { name, description, category, usage };
+    this.repondre = this.client.repondre;
+  }
+}
+module.exports = Command;
---
+++ base/command.example.js
@@ -0,0 +1,27 @@
+const Command = require('../base/Command.js');
+// Les autres modules/databases à importer
+
+class Example extends Command {
+  constructor() {
+    super({
+      name: 'commandName',
+      description: 'Une courte description',
+      usage: 'commandName <argument obligatoire> [argument optionnel]',
+      aliases: ['alias1', 'alias2'],
+      guildOnly: false,
+      enabled: true,
+      permLevel: 'User',
+      category: 'Utilitaires',
+    });
+  }
+
+  async run(message, args, level) {
+    try {
+      // Code
+    } catch (err) {
+      this.client.utils.get('error').run(err, message, this.client);
+    }
+  }
+}
+
+module.exports = Example;
