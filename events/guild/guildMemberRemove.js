250005b08ccc2841b85b0da6badfcd43598bb19c
Nino <65510604+Nino-fr@users.noreply.github.com>
2020-08-24 14:55:36 +0200

    Delete guildMemberRemove.js

--- events/guild/guildMemberRemove.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (user) => {
-  let wChannel = user.guild.channels.cache.find(
-    (r) => r.name === "bienvenue" || r.name === "bois-des-errants🌳"
-  );
-  wChannel.send(`Oh non ! ${user} a quitté le serveur...`);
-};
